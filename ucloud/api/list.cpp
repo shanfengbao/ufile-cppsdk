0c2f57a6f76ca0081a874f95518c3cd5afa058ab
gan.wu <gan.wu@ucloud.cn>
2022-07-17 19:13:26 +0800

    tmp3

--- ucloud/api/list.cpp
+++ ucloud/api/list.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <sstream>
 #include <ufile-cppsdk/common.h>
 #include <ufile-cppsdk/config.h>
 #include <ufile-cppsdk/list.h>
@@ -5,6 +9,8 @@
 #include <ufile-cppsdk/errno.h>
 #include <ufile-cppsdk/http.h>
 #include <ufile-cppsdk/urlcodec.h>
+
+#define LIST_MAX_COUNT (1000)
 
 using namespace ucloud::cppsdk::http;
 using namespace ucloud::cppsdk::utils;
@@ -16,41 +22,90 @@ namespace ucloud {
 namespace cppsdk {
 namespace api {
 
-UFileList::UFileList() : delimiter("/") {}
+UFileList::UFileList() : delimiter_("") {}
 
 UFileList::~UFileList() {}
 
 int UFileList::List(const std::string &bucket, const std::string &prefix,
                     uint32_t count, ListResultEntry *result,
-                    bool *is_truncated, std::string *next_marker) {
+                    bool *is_truncated, std::string *next_marker,
+                    const std::string &marker) {
 	int64_t ret = InitGlobalConfig();
   if (ret) {
 		return ret;
 	}
 
-	SetResource(bucket, key);
+	SetResource(bucket, prefix, count, marker);
 
   std::string signature("");
   // 构建HTTP头部
   m_http->Reset();
   m_http->AddHeader("User-Agent", USERAGENT);
-  m_http->SetURL(UFileHost(bucket) + EasyPathEscape(key));
+  m_http->AddHeader("Content-Length", std::to_string(0));
+  std::map<std::string, std::string> params;
+  params["prefix"] = prefix;
+  params["marker"] = marker;
+  params["max-keys"] = std::to_string(count);
+  params["delimiter"] = delimiter_;
+  SetURL(params);
   
   //使用 HTTP 信息构建签名
   UFileDigest digestor;
-  ret = digestor.SignWithRequest(m_http, HEAD_FIELD_CHECK, bucket, key, "",
-                                 signature);
+  ret = digestor.SignWithRequestV2(m_http, HEAD_FIELD_CHECK, bucket, "", "",
+                                 signature, "listobjects", params);
   if (ret) {
     return ret;
   }
   m_http->AddHeader("Authorization", digestor.Token(signature));
+
+  //设置输出
+  std::ostringstream oss;
+  UCloudOStream data_stream(&oss);
+  UCloudHTTPWriteParam wp = {f : NULL, os : &data_stream};
+  ret = m_http->RoundTrip(NULL, &wp, NULL);
+  if (ret) {
+    UFILE_SET_ERROR2(ERR_CPPSDK_SEND_HTTP, UFILE_LAST_ERRMSG());
+    return ERR_CPPSDK_SEND_HTTP;
+  }
+  //解析回应
+  long code = 200;
+  ret = m_http->ResponseCode(&code);
+  if (ret) {
+    UFILE_SET_ERROR(ERR_CPPSDK_CURL);
+    return ERR_CPPSDK_CURL;
+  }
+
+  std::string errmsg;
+  if (code != 200) {
+    int parse_ret = UFileErrorRsp(oss.str().c_str(), &ret, errmsg);
+    if (parse_ret) {
+      UFILE_SET_ERROR(ERR_CPPSDK_CLIENT_INTERNAL);
+      return ERR_CPPSDK_CLIENT_INTERNAL;
+    }
+    UFILE_SET_ERROR2(ret, errmsg);
+  } else {
+    std::cout << oss.str().c_str();
+  }
+  return ret;
 }
 
-void UFileDownload::SetResource(const std::string &bucket,
-                                const std::string &key) {
+void UFileList::SetResource(const std::string &bucket,
+                            const std::string &prefix,
+                            uint32_t count,
+                            const std::string &marker) {
+  bucket_ = bucket;
+  prefix_ = prefix;
+  max_key_ = count > LIST_MAX_COUNT ? LIST_MAX_COUNT : count;
+  next_marker_ = marker;
+}
 
-  m_bucket = bucket;
-  m_key = key;
+void UFileList::SetURL(std::map<std::string, std::string> params) {
+  std::string url = UFileHost(bucket_);
+  url = url + "/?listobjects";
+  for (auto it = params.begin(); it != params.end(); ++it) {
+    url = url + "&" + it->first + "=" + it->second;
+  }
+  m_http->SetURL(url);
 }
 
 } // namespace api
--- ucloud/include/ufile-cppsdk/list.h
+++ ucloud/include/ufile-cppsdk/list.h
@@ -18,7 +18,7 @@ struct ListResultEntry {
   std::string storage_class; // 文件存储类型
   uint64_t last_modified; //文件最后修改时间
   uint64_t create_time; // 文件创建时间
-}
+};
 
 typedef std::list<ListResultEntry> ListResult;
 
@@ -38,19 +38,29 @@ public:
    * 为true, 则表示返回结果由于达到count的限制而被截断
    * @next_marker: 当is_truncated为true时，next_marker被设置,
    * 下次List可以使用此值作为prefix, 实现迭代查询
+   * marker: 用于迭代查询, 如要上次查询如果被截断, 这次查询可以将
+   * 此参数设置为上次查询结果返回的next_marker, 继续查询。默认为空,
+   * 表示从头查询
    * @return: 0=成功，非0=失败
    */
   int List(const std::string &bucket, const std::string &prefix,
            uint32_t count, ListResultEntry *result,
-           bool *is_truncated, std::string *next_marker);
+           bool *is_truncated, std::string *next_marker,
+           const std::string &marker = "");
 
 private:
-  void SetResource(const std::string &bucket, const std::string &key);
+  void SetResource(const std::string &bucket, const std::string &prefix,
+      uint32_t count, const std::string &marker);
 
-  std::string m_bucket;
-  std::string m_key;
-  std::string next_marker;
-  std::string delimiter;
+  void SetURL(std::map<std::string, std::string> params);
+
+  // int ParseRsp(const char *body, int64_t *retcode, std::string &errmsg);
+
+  std::string bucket_;
+  std::string prefix_;
+  std::string next_marker_;
+  std::string delimiter_;
+  uint32_t max_key_;
 };
 
 } // namespace api
